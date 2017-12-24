53a25fa1c749f1bd8f1ca190f71ddb0290cc4778
Sachin MS <savigms2@gmail.com>
2017-12-24 12:13:35 +0530

    need to start from fresh

--- config.js
+++
@@ -1,3 +0,0 @@
-export const nameLibrary = "ionic-hide-fab-onscroll";
-export const PATH_SRC = "src/";
-export const PATH_DIST = "dist/";
--- rollup.config.esm.js
+++
@@ -1,5 +0,0 @@
-import config from './rollup.config.umd.js';
-import { nameLibrary, PATH_DIST } from './config.js'
-config.output.format = "es";
-config.output.file = PATH_DIST + nameLibrary + ".esm.js";
-export default config;
